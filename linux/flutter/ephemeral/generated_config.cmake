679e552327c2fee8dd6ab787cf7ea04fa2eeadd7
Fabrizio Paolini <92989222+St0rmagedd0n@users.noreply.github.com>
2021-10-27 08:58:03 +0200

    Add files via upload

--- linux/flutter/ephemeral/generated_config.cmake
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -1,15 +1,14 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "/home/fabrizio/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/opt/flutter" FLUTTER_ROOT)
 file(TO_CMAKE_PATH "/home/fabrizio/StudioProjects/macchinapi" PROJECT_DIR)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=/home/fabrizio/snap/flutter/common/flutter"
+  "FLUTTER_ROOT=/opt/flutter"
   "PROJECT_DIR=/home/fabrizio/StudioProjects/macchinapi"
-  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
-  "TREE_SHAKE_ICONS=false"
+  "TREE_SHAKE_ICONS=true"
   "PACKAGE_CONFIG=/home/fabrizio/StudioProjects/macchinapi/.dart_tool/package_config.json"
-  "FLUTTER_TARGET=/home/fabrizio/StudioProjects/macchinapi/lib/main.dart"
+  "FLUTTER_TARGET=lib/main.dart"
 )
